216a623a40448a077d9755b9be6732a99eccb0cd
SASE <zmtt@outlook.com>
2016-07-08 18:35:46 +0800

    Initial Commit

---
+++ MYDOTA2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MYDOTA2
+//
+//  Created by SASE on 7/8/16.
+//  Copyright © 2016 SASE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MYDOTA2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MYDOTA2
+//
+//  Created by SASE on 7/8/16.
+//  Copyright © 2016 SASE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
